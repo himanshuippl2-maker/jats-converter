ef5ee6baad5263b1a0c9e7a7a8f7df3707761f6f
himanshuippl2-maker <himanshuippl2@gmail.com>
2026-02-27 13:36:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -400,4 +400,5 @@ def health():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    print(f'Starting on port {port}', flush=True)
     app.run(host='0.0.0.0', port=port, debug=False)
